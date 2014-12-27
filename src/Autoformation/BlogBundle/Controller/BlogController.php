3be09a5ac6e6700e2c405fabd99f161c1245c55e
etanok <oumar.konate@atexo.com>
2014-12-27 22:19:49 +0000

    Utilisation de l'heritage des templates

--- src/Autoformation/BlogBundle/Controller/BlogController.php
+++ src/Autoformation/BlogBundle/Controller/BlogController.php
@@ -19,7 +19,32 @@ class BlogController extends Controller
         }
         // Ici, on récupérera la liste des articles, puis on la passera au template
         // Mais pour l'instant, on ne fait qu'appeler le template
-        return $this->render('AutoformationBlogBundle:Blog:index.html.twig');
+        // Les articles :
+        $articles = array(
+            array(
+                'titre' => 'Mon weekend a Phi Phi Island !',
+                'id' => 1,
+                'auteur' => 'winzou',
+                'contenu' => 'Ce weekend était trop bien. Blabla…',
+                'date' => new \Datetime()
+            ),
+            array(
+                'titre' => 'Repetition du National Day de Singapour',
+                'id' => 2,
+                'auteur' => 'winzou',
+                'contenu' => 'Bientôt prêt pour le jour J. Blabla…',
+                'date' => new \Datetime()
+            ),
+            array(
+                'titre' => 'Chiffre d\'affaire en hausse',
+                'id' => 3,
+                'auteur' => 'M@teo21',
+                'contenu' => '+500% sur 1 an, fabuleux. Blabla…',
+                'date' => new \Datetime()
+            )
+        );
+
+        return $this->render('AutoformationBlogBundle:Blog:index.html.twig', array('articles' => $articles));
     }
     public function voirAction($id)
     {
@@ -51,5 +76,19 @@ class BlogController extends Controller
         // Ici, on récupérera l'article correspondant à $id
         // Ici, on gérera la suppression de l'article en question
         return $this->render('AutoformationBlogBundle:Blog:supprimer.html.twig');
+    }
+    public function menuAction($nombre) // Ici, nouvel argument $nombre, on l'a transmis via le render() depuis la vue
+    {
+        // On fixe en dur une liste ici, bien entendu par la suite on la récupérera depuis la BDD !
+        // On pourra récupérer $nombre articles depuis la BDD,
+        // avec $nombre un paramètre qu'on peut changer lorsqu'on appelle cette action
+        $liste = array(
+            array('id' => 2, 'titre' => 'Mon dernier weekend !'),
+            array('id' => 5, 'titre' => 'Sortie de Symfony2.1'),
+            array('id' => 9, 'titre' => 'Petit test')
+        );
+        return $this->render('AutoformationBlogBundle:Blog:menu.html.twig', array(
+            'liste_articles' => $liste // C'est ici tout l'intérêt : le contrôleur passe les variables nécessaires au template !
+        ));
     }
 }
