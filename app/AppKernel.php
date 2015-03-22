f27e4696081ddc5f02253bef353c0b151849f022
Oumar KONATE <konatoumar@gmail.com>
2015-03-22 20:47:43 +0000

    Configuration de la securite

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Autoformation\BlogBundle\AutoformationBlogBundle(),
+            new Autoformation\UserBundle\AutoformationUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
